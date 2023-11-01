dc94337ae6efba4220c92de2ba4e7afa1250c94e
Abrar-test <thesketch506@gmail.com>
2023-11-01 19:46:09 +0530

    Added models to store username , random number , usernumber , chances to show and perform multipler operatio over database

--- Guessing_game_App/models.py
+++ Guessing_game_App/models.py
@@ -1,2 +1,8 @@
 from django.db import models
 
+class info(models.Model):
+  username = models.CharField(max_length=255)
+  usernumber=models.IntegerField()
+  systemnumber=models.IntegerField()
+  chance=models.IntegerField()
+  
