3d1858331585e429ad5b72850a8c89e257e204ba
Abrar-test <thesketch506@gmail.com>
2023-11-02 18:39:09 +0530

    Modified Views and Urls to reduce redundancy

--- Guessing_game_App/urls.py
+++ Guessing_game_App/urls.py
@@ -2,9 +2,9 @@ from django.urls import path
 from Guessing_game_App import views
 urlpatterns = [
     path('',views.home ),
-    path('guess',views.guess ),
-    path('number',views.number ),
-    path('validate',views.validate ),
-    path('retry',views.retry ),
-    path('validateretry',views.validateretry ),
-]
+    path('usernameentry',views.usernameentry),
+    path('rangeentry',views.rangeentry),
+    path('numberentry',views.numberentry),
+    path('validate',views.validate),
+    ]
+    
--- Guessing_game_App/views.py
+++ Guessing_game_App/views.py
@@ -1,66 +1,52 @@
 from django.shortcuts import render
-from .models import info
+from .models import username,userrangeandchance,usernumber
 from django.http import HttpResponse
 import random 
+from math import log2
 
 # Create your views here.
 def home(request):
-    return render(request , "homepage.html")
-def guess(request):
-    context={"chance":7}
-    return render(request,"guess.html", context)
-def number(request):
+    return render(request , "1_homepage.html")
+def usernameentry(request):
+    return render(request , "2_usernameentry.html")
+def rangeentry(request):
     if request.method=="POST":
-        username=request.POST["username"]
-        number=request.POST["number"]
-        chance=7
-        randonumber=random.randint(1,100)
-        u_name=info(username=username, usernumber=number,systemnumber=randonumber , chance=chance)
+        username_input=request.POST["username"]
+        u_name=username(user_name = username_input)
         u_name.save()
-        context={"username":username,"number":number ,"chance":chance}
-        return render(request,"number.html",context)
+        context={"username":username_input}
+        return render(request,"3_rangeentry.html",context)
     else:
          return HttpResponse("Error")
+def numberentry(request):
+    if request.method=="POST":
+        l1=int(request.POST["lowerrange"])
+        l2=int(request.POST["upperrange"])
+        c=int(log2(l2-l1)+1)
+        randomnumber=random.randint(l1,l2)
+        u_range=userrangeandchance(lowerrange=l1,upperrange=l2,chance=c,systemnumber=randomnumber)
+        u_range.save()
+        context={"lowerrange":l1,"upperrange":l2,"chance":c}
+        return render(request,"3_showrangeandchance.html",context)
+    else:
+         return HttpResponse("Error")
+    
 def validate(request):
     if request.method=="POST":
-        chance=info.objects.values().latest("id")["chance"]
+        u_number=int(request.POST["unum"])
+        unumber=usernumber(usernumber=u_number)
+        unumber.save()
+        u_name=username.objects.values().latest("id")["user_name"]
+        systemnumber=userrangeandchance.objects.values().latest("id")["systemnumber"]
+        chance=userrangeandchance.objects.values().latest("id")["chance"]
+        print(systemnumber)
+        l1=userrangeandchance.objects.values().latest("id")["lowerrange"]
+        l2=userrangeandchance.objects.values().latest("id")["upperrange"]
         chance=chance-1
-        usernumber=info.objects.values().latest("id")["usernumber"]
-        username=info.objects.values().latest("id")["username"]
-        systemnumber=info.objects.values().latest("id")["systemnumber"]
-        usernumber=info.objects.values().latest("id")["usernumber"]
-        u_name=info(username=username, usernumber=usernumber,systemnumber=systemnumber, chance=chance)
-        u_name.save()
-        context={"username":username , "systemnumber":systemnumber,"usernumber":usernumber,"chance":chance}
+        
+        range_s=userrangeandchance(systemnumber=systemnumber, chance=chance ,lowerrange=l1,upperrange=l2)
+        range_s.save()
+        context={"username":u_name , "systemnumber":systemnumber,"usernumber":u_number,"chance":chance}
         return render(request,"validate.html",context)
     else:
          return HttpResponse("Error")
-def retry(request):
-        chance=info.objects.values().latest("id")["chance"]
-        chance=chance-1
-        usernumber=info.objects.values().latest("id")["usernumber"]
-        username=info.objects.values().latest("id")["username"]
-        systemnumber=info.objects.values().latest("id")["systemnumber"]
-        usernumber=info.objects.values().latest("id")["usernumber"]
-        u_name=info(username=username, usernumber=usernumber,systemnumber=systemnumber, chance=chance)
-        u_name.save()
-        context={"username":username , "systemnumber":systemnumber,"usernumber":usernumber,"chance":chance}
-        return render(request,"retry.html",context)
-def validateretry(request):
-    if request.method=="POST":
-        number=request.POST["number"]
-        chance=info.objects.values().latest("id")["chance"]
-        username=info.objects.values().latest("id")["username"]
-        systemnumber=info.objects.values().latest("id")["systemnumber"]
-        u_name=info(username=username, usernumber=number,systemnumber=systemnumber, chance=chance)
-        u_name.save()
-      
-        usernumber=info.objects.values().latest("id")["usernumber"]
-        context={"username":username , "systemnumber":systemnumber,"usernumber":usernumber ,"chance":chance}
-        return render(request,"validate.html",context)
-    else:
-         return HttpResponse("Error")
-
-
-
-
